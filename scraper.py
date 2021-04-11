9509a34db0faf70e55534818ea6af36549d21ef7
João Costa <worldoftanksreviewer@gmail.com>
2021-04-11 14:04:33 +0100

    finished scraping main page for course info, nr, school nr and bchlrs/mstrs

--- scraper.py
+++ scraper.py
@@ -23,14 +23,28 @@ def main():
     sectionHeaders = soup.findAll(attrs={'class': 'box9'})
     for sectHeader in sectionHeaders:
         currentSection = sectHeader.findChildren(attrs={'class': 'lin-area-d2'})
-        print("\n" + currentSection[0].text + "\n")
+        schoolName = currentSection[0].text
         for sibling in sectHeader.findNextSiblings():
             if('class' in sibling.attrs):
-                if "box9" in sibling.attrs["class"]:
+                if "box9" in sibling.attrs["class"] or "br" in sibling.name or "nobottomgap" in sibling.attrs["class"]:
                     break
-                courseName = sibling.findChildren("a")
-                for child in courseName:
-                    print(child.getText())
+                subDivs = sibling.findChildren()
+
+                courseNumber = subDivs[1].text
+                courseURL = "https://www.dges.gov.pt/guias/"+subDivs[2].next["href"]
+                courseName = subDivs[3].text
+                courseType = subDivs[4].text
+                #courseName = sibling.findChildren("a")
+                #courseDataURL = courseName[0]["href"]
+                #print(courseName[0].getText())
+                #print(courseDataURL)
+
+                print("\n"+schoolName)
+                print(courseName)
+                print(courseNumber)
+                print(courseType)
+                print(courseURL)
+                
             
             
 
