e69db1c1ba560ba5899a3066362647c2ea063e39
João Costa <worldoftanksreviewer@gmail.com>
2021-04-11 15:41:11 +0100

    added parsing of xlsx file for unemployment rate. now everything is scraped and put onto a txt file. next step is putting it into an sql database

--- scraper.py
+++ scraper.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import xlrd
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
+from openpyxl import load_workbook
 #from typing import namedtuple
 
 
@@ -12,17 +16,23 @@ import time
 #    tipoCurso : str
 
 def main():
-    print("test")
+    excelPath = "excel.xls"
+    wb = load_workbook(filename = 'DB.xlsx')
+    ws = wb.active
+    
+    #print("test")
     downloadURL = "https://www.dges.gov.pt/guias/indest.asp"
     data = requests.get(downloadURL)
-    print(data)
+    #print(data)
     soup = BeautifulSoup(data.text, 'html.parser')
     courses = list(soup.findAll(attrs={'class': 'lin-ce'}))
-    print (len(courses))
-
+    #print (len(courses))
+    f = open("data.txt", "w")
     sectionHeaders = soup.findAll(attrs={'class': 'box9'})
     for sectHeader in sectionHeaders:
         currentSection = sectHeader.findChildren(attrs={'class': 'lin-area-d2'})
+        currentSection2 = sectHeader.findChildren(attrs={'class': 'lin-area-c1'})
+        schoolNumber = currentSection2[0].text
         schoolName = currentSection[0].text
         for sibling in sectHeader.findNextSiblings():
             if('class' in sibling.attrs):
@@ -39,13 +49,64 @@ def main():
                 #print(courseName[0].getText())
                 #print(courseDataURL)
 
-                print("\n"+schoolName)
-                print(courseName)
-                print(courseNumber)
-                print(courseType)
-                print(courseURL)
-                
             
+                #print("\n"+schoolName)
+                #print(courseName)
+                #print(courseNumber)
+                #print(courseType)
+                #print(courseURL)
+
+                courseData = requests.get(courseURL)
+                courseSoup = BeautifulSoup(courseData.text, 'html.parser')
+                dataCells = list(courseSoup.findAll(attrs={'class': 'tvag'}))
+
+                firstPhaseGPA2019 = 0
+                secondPhaseGPA2019 = 0
+                secondPhaseGPA2020 = 0
+                firstPhaseGPA2020 = 0
+
+                if(len(dataCells) != 0):
+                    firstPhaseGPA2020 = dataCells[10].text
+                    secondPhaseGPA2020 = dataCells[11].text
+                    firstPhaseGPA2019 = dataCells[8].text
+                    secondPhaseGPA2019 = dataCells[9].text
+
+                #print(firstPhaseGPA2019)
+                #print(secondPhaseGPA2019)
+                #print(firstPhaseGPA2020)
+                #print(secondPhaseGPA2020)
+
+                infoCursosURL = f"https://infocursos.mec.pt/dges.asp?code={schoolNumber}&codc={courseNumber}"
+                txDesemprego = 100
+                for row in ws.values:
+                    if((row[7] == courseNumber or row[6] == courseName) and (row[1] == schoolNumber or row[3] == schoolNumber)):
+                        txDesemprego = row[12]
+                print(txDesemprego)
+
+                f.write("\n\n"+schoolName)
+                f.write("\n"+schoolNumber)
+                f.write("\n"+courseName)
+                f.write("\n"+courseNumber)
+                f.write("\n"+courseType)
+                f.write("\n"+"2019f1: "+str(firstPhaseGPA2019))
+                f.write("\n"+"2019f2: "+str(secondPhaseGPA2019))
+                f.write("\n"+"2020f1: "+str(firstPhaseGPA2020))
+                f.write("\n"+"2020f2: "+str(secondPhaseGPA2020))
+                f.write("\n"+str(txDesemprego))
+                
+                #print (infoCursosURL)
+
+                #for row_num in range(sheet.nrows):
+                #    row_value = sheet.row_values(row_num)
+                #    print(row_value[0])
+                #    print(row_value[6])
+                #    if row_value[0] == schoolNumber and row_value[6] == courseNumber:
+                #        print (row_value[11])
+
+                #statCourseData = requests.get(infoCursosURL)
+                #statCourseSoup = BeautifulSoup(statCourseData.text, 'html.parser')
+                #dataCells = list(statCourseSoup.findAll(attrs={'class': 'tvag'}))
+    f.close()
             
 
 
